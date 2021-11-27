55307f78700c040485860aaf19637a614f7aa8fc
silviajboo <sljboo@gmail.com>
2021-11-27 23:33:12 +0100

    Added controls component. Added favicons, title and meta description. Modified some media query styles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 import '../scss/styles.scss';
+import touchable from './utilities/touchable';
+
+const isTouchable = touchable();
+document.documentElement.classList.add(isTouchable ? 'touch' : 'non-touch');
+
+
---
+++ assets/js/utilities/touchable.js
@@ -0,0 +1,7 @@
+function touchable() {
+    const touchsupport = ('ontouchstart' in window) || window.navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
+    
+    return !!touchsupport;
+}
+
+export default touchable;
